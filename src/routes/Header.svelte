f1313837722e7c34e64ba53b67f15d20899ef863
Paul2D <paul.maxineanu@gmail.com>
2023-01-30 18:24:38 +0200

    add exercise-library route

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <header>
-	<div class="corner bg-green-800">
+	<div class="corner">
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
 		</a>
@@ -19,11 +19,11 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+			<!-- <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			</li> -->
+			<li aria-current={$page.url.pathname.startsWith('/exercise-library') ? 'page' : undefined}>
+				<a href="/exercise-library">Library</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
---
+++ src/routes/exercise-library/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Exercise library</title>
+	<meta name="description" content="library" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Exercise Library</h1>
+
+	<p>form with add exercise</p>
+
+	<p>filter exercise</p>
+
+	<p>list exercises</p>
+</div>
