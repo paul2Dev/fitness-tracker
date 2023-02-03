9b3c15881473ae03da2d82761618a4fa516e6890
Paul2D <paul.maxineanu@gmail.com>
2023-02-03 18:04:22 +0200

    add store with muscleGroups and exercises

--- src/App.svelte
+++ src/App.svelte
@@ -13,9 +13,8 @@
     <a href="#" on:click={() => (activeTab = Library)} class="ml-6 text-white font-medium hover:text-orange-200 {activeTab === Library ? 'text-orange-200' : ''}">Library</a>
   </nav>
 
-  <div class="container mx-auto h-screen flex justify-center py-10 bg-orange-100">
+  <div class="container mx-auto flex justify-center py-10 bg-orange-100">
     <div class="w-full p-6"><svelte:component this={activeTab} /></div>
-    
   </div>
   
 </main>
