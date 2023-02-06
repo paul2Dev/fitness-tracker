21b0a01cf318119418a0e35a7fc1fe1f14f9ead9
Paul2D <paul.maxineanu@gmail.com>
2023-02-06 10:49:57 +0200

    save exercise and update store

--- src/components/Library.svelte
+++ src/components/Library.svelte
@@ -1,17 +1,37 @@
 <script>
   import { muscleGroups, exercises } from '../store/stores.js';
+  const formValues = {
+    name: '',
+    muscleGroup: '',
+    videoUrl: ''
+  };
+
+  function saveExercise() {
+    exercises.update(exercises => {
+      exercises.push({
+        name: formValues.name,
+        muscleGroup: formValues.muscleGroup,
+        videoUrl: formValues.videoUrl
+      });
+      return exercises;
+    });
+  };
+
 </script>
 
 <div class="bg-white p-6 rounded-lg shadow-md">
-    <form class="flex flex-col md:flex-row">
-      <input type="text" class="shadow-sm p-2 rounded-lg border border-teal-600 mb-2 md:mb-0 md:mr-2 w-full md:w-1/3" placeholder="Exercise Name">
-      <select class="shadow-sm p-2 rounded-lg border border-teal-600 mb-2 md:mb-0 md:mr-2 w-full md:w-1/3">
+    <form class="flex flex-col md:flex-row" on:submit|preventDefault={saveExercise}>
+      <input bind:value={formValues.name} type="text" class="shadow-sm p-2 rounded-lg border border-teal-600 mb-2 md:mb-0 md:mr-2 w-full md:w-1/3" placeholder="Exercise Name">
+      <select bind:value="{formValues.muscleGroup}" class="shadow-sm p-2 rounded-lg border border-teal-600 mb-2 md:mb-0 md:mr-2 w-full md:w-1/3">
         <option value="">All Muscle Groups</option>
         {#each $muscleGroups as muscleGroup, index}
           <option value={muscleGroup.id}>{muscleGroup.name}</option>
         {/each}
       </select>
-      <input type="text" class="shadow-sm p-2 rounded-lg border border-teal-600 mb-2 md:mb-0 md:mr-2 w-full md:w-1/3" placeholder="Video URL">
+      <input bind:value="{formValues.videoUrl}" type="text" class="shadow-sm p-2 rounded-lg border border-teal-600 mb-2 md:mb-0 md:mr-2 w-full md:w-1/3" placeholder="Video URL">
+      <button class="bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">
+        submit
+      </button>
     </form>
     <table class="mt-6 w-full">
       <thead>
@@ -26,11 +46,9 @@
               <tr>
                 <td class="p-2 border border-teal-600">{exercise.name}</td>
                 <td class="p-2 border border-teal-600">{$muscleGroups.find(group => group.id === exercise.muscleGroup).name}</td>
-                <td class="p-2 border border-teal-600">{#if 'videoUrl' in exercise} <a href="{exercise.videoUrl}" target="_blank">{exercise.videoUrl}</a> {/if}</td>
+                <td class="p-2 border border-teal-600">{#if 'videoUrl' in exercise} <a href="{exercise.videoUrl}" target="_blank" rel="noreferrer">{exercise.videoUrl}</a> {/if}</td>
               </tr>
             {/each}
-            
-
         </tbody>
     </table>
 </div>
