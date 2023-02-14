a7c7d1f3821d6555a9fc36e397b2dac006a09b56
Paul2D <paul.maxineanu@gmail.com>
2023-02-14 16:05:23 +0200

    add svelte DateInput to workout form

--- src/components/Workout.svelte
+++ src/components/Workout.svelte
@@ -1,24 +1,13 @@
 <script>
     import { exercises } from '../store/stores.js';
-    import { onMount } from 'svelte';
-    
+    import { DateInput } from 'date-picker-svelte'
     import { FloatingLabelInput, Button, Select } from 'flowbite-svelte';
-    import Datepicker from 'flowbite-datepicker/Datepicker';
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
-
-
-    let datepicker;
-
-    onMount(() => {
-        datepicker = new Datepicker(document.getElementById('datepicker'), {
-            format: 'dd/mm/yyyy',
-        }); 
-    });
+    import { Table, TableBody, TableBodyCell, TableBodyRow } from 'flowbite-svelte';
 
     let formValues = {
         name: '',
         description: '',
-        created_at: '',
+        created_at: new Date(),
         exercises: [
         {
             id: 0,
@@ -55,6 +44,7 @@
         formValues.exercises = [...formValues.exercises.slice(0, index), ...formValues.exercises.slice(index + 1)];
     }
 
+
     function saveWorkout() {
         console.log(formValues);
     }
@@ -71,17 +61,12 @@
                 <FloatingLabelInput  size="small" bind:value={formValues.description} style="outlined" type="text" id="workout_description" label="Workout Description" required />
             </div>
             <div>
-                <div class="relative max-w-sm">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                    </div>
-                    <input bind:value={formValues.created_at}  id="datepicker"  type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
-                  </div>
+                <DateInput bind:value={formValues.created_at} format="dd/mm/yyyy" />
             </div>
         </div>
 
         <Table>
-            <TableBody class="divide-y">
+            <TableBody>
             {#each formValues.exercises as exercise, i}
               <TableBodyRow>
                 <TableBodyCell><Select size="md" items={selectExercises} bind:value="{exercise.exerciseID}" required /></TableBodyCell>
